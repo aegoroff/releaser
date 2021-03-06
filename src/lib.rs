43b00d8b2c095d6a9dd6272597436d23f8c653fc
egoroff <egoroff@gmail.com>
2021-03-06 09:41:11 +0300

    workspace config parsing prototype

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,39 @@
+use serde::{Deserialize};
 
 extern crate semver;
 extern crate toml;
+extern crate serde;
 
+#[derive(Deserialize)]
+struct WorkspaceConfig {
+    workspace: Workspace,
+}
+
+#[derive(Deserialize)]
+struct Workspace {
+    members: Vec<String>,
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn toml_parse_workspace() {
+        // Arrange
+        let t = r#"
+[workspace]
+
+members = [
+    "solv",
+    "solp",
+]
+        "#;
+
+        // Act
+        let cfg: WorkspaceConfig = toml::from_str(t).unwrap();
+
+        // Assert
+        assert_eq!(2, cfg.workspace.members.len());
     }
 }
