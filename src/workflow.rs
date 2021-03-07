45b8f0da7961577795aa3fa4836511645ef04b5b
egoroff <egoroff@gmail.com>
2021-03-07 22:37:28 +0300

    delay added between crates publishing

--- src/workflow.rs
+++ src/workflow.rs
@@ -3,6 +3,7 @@ use crate::cargo;
 use crate::{Increment, VersionIter};
 use std::path::PathBuf;
 use vfs::PhysicalFS;
+use std::{thread, time};
 
 pub fn release(path: &str, incr: Increment) -> crate::Result<()> {
     let root_path = PathBuf::from(path);
@@ -15,9 +16,13 @@ pub fn release(path: &str, incr: Increment) -> crate::Result<()> {
     let commit_msg = format!("New release {}", &ver);
     git::commit(&commit_msg, path)?;
 
+    let minute = time::Duration::from_secs(60);
     let crates_to_publish = it.topo_sort();
-    for publish in &crates_to_publish {
-        while cargo::publish(path, publish).is_err() {}
+    for (i, publish) in crates_to_publish.iter().enumerate() {
+        cargo::publish(path, publish)?;
+        if i < crates_to_publish.len() - 1 {
+            thread::sleep(minute);
+        }
     }
 
     git::create_tag(path, &ver)?;
